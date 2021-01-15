47f4861016891b61b608071cd468f5e69370acde
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2021-01-15 10:27:51 -0500

    [cattoys] Add forms index and show page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Cat
+from .models import Cat, CatToy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # main_app/views.py
 
@@ -14,4 +17,67 @@ def about(request):
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url ='/cats'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
+
+
+### USER ###
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = Cat.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'cats': cats})
+
+
+### CatToy ###
+def cattoys_index(request):
+    cattoys = CatToy.objects.all()
+    return render(request, 'cattoys/index.html', {'cattoys': cattoys})
+
+def cattoys_show(request, cattoy_id):
+    cattoy = CatToy.objects.get(id=cattoy_id)
+    return render(request, 'cattoys/show.html', {'cattoy': cattoy })
+
+
+class CatToyCreate(CreateView):
+    model = CatToy
+    fields = '__all__'
+    success_url ='/cattoys'
+
+class CatToyUpdate(UpdateView):
+    model = CatToy
+    fields = ['name', 'color']
+    success_url ='/cattoys'
+
+class CatToyDelete(DeleteView):
+    model = CatToy
+    success_url ='/cattoys'
+
+
+
 
